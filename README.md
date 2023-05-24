2c86109fad67be64001b0a245f2b426af137fee7
Futyn-Maker <github@yakuboy.ru>
2023-05-24 13:05:53 +0300

    Initial version

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # RuthLemm
-A transformer BART-based lemmatizer for ruthenian language
+
+RuthLemm is a transformer BART-based lemmatizer for Ruthenian language. With its help, you can lemmatize treebanks in the Universal Dependencies format (UD).
+
+## Installation
+
+1. Clone this repo: `git clone https://github.com/Futyn-Maker/RuthLemm.git`;
+2. Move to the repo folder: `cd ruthlemm`;
+3. Install dependencies: `pip3 install -r requirements.txt`.
+
+## Usage
+
+Ruthlemm is a command line utility. As the first argument, it takes the path to the file in the `.conllu` format, and as the second argument - the path to the output file where the lemmatized corpus will be written.
+
+The `--morphology` (`-m`) flag is also available - set it if your data contains morphological markup and you want to improve lemmatization by using it.
+
+## Examples
+
+`python -m ruthlemm in.conllu out.conllu` - lemmatizes the corpus in `in.conllu` without relying on morphological markup and writes the data in `out.conllu`.
+
+`python -m ruthlemm -m in.conllu out.conllu` - lemmatizes the corpus in `in.conllu`, using morphology information, and writes the data to 'out.conllu'.
---
+++ ruthlemm.py
@@ -0,0 +1,76 @@
+from itertools import cycle
+import random
+import argparse
+
+from simpletransformers.seq2seq import Seq2SeqModel
+import pandas as pd
+import torch
+
+
+random.seed = 42
+
+def load_conllu_dataset(datafile, join=False):
+    arr = []
+    with open(datafile, encoding='utf-8') as inp:
+        strings = inp.readlines()
+    for s in strings:
+        if (s[0] != "#" and s.strip()):
+            split_string = s.split('\t')
+            if split_string[1] == "(" or split_string[1] == ")" or split_string[1] == "[" or split_string[1] == "]":
+                form = split_string[1]
+            else:
+                form = split_string[1].replace("(", "").replace(")", "").replace("[", "").replace("]", "")
+            if split_string[3] != "PROPN":
+                form = form.lower()
+            else:
+                form = form.capitalize()
+            lemma = split_string[2]
+            if split_string[3] == "PROPN":
+                lemma = lemma.capitalize()
+            if join:
+                inpt = form + " " + split_string[3] + " " + split_string[5]
+            else:
+                inpt = form
+            pos = split_string[3]
+            arr.append([inpt, lemma, pos])
+    return pd.DataFrame(arr, columns=["input_text", "target_text", "pos"])
+
+def predict(in_file, out_file, join=False):
+    if join:
+        model_name = "Futyn-Maker/RuthLemm-morphology"
+    else:
+        model_name = "Futyn-Maker/RuthLemm"
+
+    model = Seq2SeqModel(
+        encoder_decoder_type="bart",
+        encoder_decoder_name=model_name,
+        use_cuda=torch.cuda.is_available()
+    )
+
+    pred_data = load_conllu_dataset(in_file, join=join)["input_text"].tolist()
+    predictions = cycle(model.predict(pred_data))
+
+    with open(in_file, encoding="utf8") as inp:
+        strings = inp.readlines()
+    predicted = []
+    for s in strings:
+        if (s[0] != "#" and s.strip()):
+            split_string = s.split("\t")
+            split_string[2] = next(predictions)
+            joined_string = "\t".join(split_string)
+            predicted.append(joined_string)
+            continue
+        predicted.append(s)
+
+    with open(out_file, "w", encoding="utf8") as out:
+        out.write("".join(predicted))
+
+if __name__ == '__main__':    
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input_file", type=str, help="Path to the input file")
+    parser.add_argument("output_file", type=str, help="Path to the output file")
+    parser.add_argument("--morphology", "-m", action="store_true", help="Use morphology")
+
+    args = parser.parse_args()
+    predict(args.input_file, args.output_file, args.morphology)
+    print("All done!")
